af6b95c857de574bc8696edef17d52367e7b1480
anlkamber3 <105453976+anlkamber3@users.noreply.github.com>
2023-09-12 13:55:23 +0300

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ os.chdir('/home/anl/Desktop/nanosim')
 
 #Topology 
 
-### EDIT HERE ONLY
+
 #Distribution Plot
 plt.plot(domains.distribution_time_domain, functions.cuda_distribution_plotter("Topology"),label= '$K = Half-Space CUDA Simulation (1cm Reflective Sphere Radius)$')
 #plt.plot(domains.distribution_time_domain,functions.analytic_distribution(2),label= '$K = Approximation$')
@@ -20,7 +20,7 @@ plt.ylabel("hitting rate")
 #Cumulative Distribution Plot
 #plt.plot(domains.cumulative_distribution_time_domain,functions.cuda_cumulative_distribution_plotter("Topology"),label= '$K = Half-Space CUDA Simulation (1cm Reflective Sphere Radius)$')
 #plt.plot(domains.cumulative_distribution_time_domain,functions.analytic_cumulative(2),label= '$K = Approximation$')
-### DON'T EDIT BELOW 
+
 
 print("The phi equals to:" + str(geometry.phi))
 
