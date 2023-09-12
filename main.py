68f707546b1438f60d7e1669e434fea5ce0e37e7
anlkamber3 <105453976+anlkamber3@users.noreply.github.com>
2023-09-12 13:54:40 +0300

    Update main.py

--- main.py
+++ main.py
@@ -12,8 +12,10 @@ os.chdir('/home/anl/Desktop/nanosim')
 ### EDIT HERE ONLY
 #Distribution Plot
 plt.plot(domains.distribution_time_domain, functions.cuda_distribution_plotter("Topology"),label= '$K = Half-Space CUDA Simulation (1cm Reflective Sphere Radius)$')
-plt.plot(domains.distribution_time_domain,functions.analytic_distribution(2),label= '$K = Approximation$')
-
+#plt.plot(domains.distribution_time_domain,functions.analytic_distribution(2),label= '$K = Approximation$')
+plt.title("Hitting rate")
+plt.xlabel("time") 
+plt.ylabe("hitting rate")
 #Cumulative Distribution Plot
 #plt.plot(domains.cumulative_distribution_time_domain,functions.cuda_cumulative_distribution_plotter("Topology"),label= '$K = Half-Space CUDA Simulation (1cm Reflective Sphere Radius)$')
 #plt.plot(domains.cumulative_distribution_time_domain,functions.analytic_cumulative(2),label= '$K = Approximation$')
