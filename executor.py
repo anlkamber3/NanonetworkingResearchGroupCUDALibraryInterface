55edf5e9f95a4b431ab0397ed0f687e463b7e5bd
anlkamber3 <105453976+anlkamber3@users.noreply.github.com>
2023-09-12 12:41:53 +0300

    Create executor.py

---
+++ executor.py
@@ -0,0 +1,27 @@
+# This script runs simulations. You can adjust the number of them and the names.
+
+import os
+
+name_of_simulation = "Topology"
+
+number_of_simulations = 100
+
+os.chdir('/home/anl/Desktop/nanosim') #It is the path of the location of CUDA Library
+
+os.system('cmake .')
+
+os.system('make')
+
+for i in range(number_of_simulations):
+    os.system(f'./sim_benchmark.exe > {name_of_simulation}{i}')
+
+#You can visualize one of the simulation results to see how simulation works.
+
+#simulation_To_Visualize = "Topology1"
+
+#os.system(f'./visualize.py {simulation_To_Visualize}')
+
+quit()
+
+
+
