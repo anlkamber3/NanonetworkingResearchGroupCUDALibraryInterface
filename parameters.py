0ae16329c3cd27b99bc73a38462686e7a1f04860
anlkamber3 <105453976+anlkamber3@users.noreply.github.com>
2023-09-12 12:41:11 +0300

    Create parameters.py

---
+++ parameters.py
@@ -0,0 +1,27 @@
+# Parameters
+
+# All the units of parameters are SI units.
+
+D = 79.4*(10**-12)
+
+total_molecules = 10**6
+
+time_step = 10**(-4)  # Time Resolution
+
+t = 2  # Simulation Run Time
+
+number_of_steps = t/time_step
+
+step_Size = 50  # Scaling when plotting the distribution.
+
+number_of_simulations = 100
+
+# Half Space Analytic(It is assumed that reflective wall is always yz plane.)
+
+location_Of_Point_Transmitter = (0, 0, (10**(-5)))
+location_Of_Spherical_Absorber = ((10**(-5)), 0, 0)
+
+# Receiver:
+
+r_i = 5*(10**(-6))
+
