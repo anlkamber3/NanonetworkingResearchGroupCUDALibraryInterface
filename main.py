46f4c682f940bee4f90ad1b256b32e9cef748867
anlkamber3 <105453976+anlkamber3@users.noreply.github.com>
2023-09-12 13:49:44 +0300

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ plt.plot(domains.distribution_time_domain, functions.cuda_distribution_plotter("
 plt.plot(domains.distribution_time_domain,functions.analytic_distribution(2),label= '$K = Approximation$')
 
 #Cumulative Distribution Plot
-#plt.plot(domains.cumulative_distribution_time_domain,functions.cuda_cumulative_distribution_plotter("topology_zero"),label= '$K = Half-Space CUDA Simulation (1cm Reflective Sphere Radius)$')
+#plt.plot(domains.cumulative_distribution_time_domain,functions.cuda_cumulative_distribution_plotter("Topology"),label= '$K = Half-Space CUDA Simulation (1cm Reflective Sphere Radius)$')
 #plt.plot(domains.cumulative_distribution_time_domain,functions.analytic_cumulative(2),label= '$K = Approximation$')
 ### DON'T EDIT BELOW 
 
